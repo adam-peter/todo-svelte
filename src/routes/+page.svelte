57fd6a50c1794085c7e719e59ddc64e1ec48b691
adam.petrzela <adam.petrzela@ais-servis.cz>
2023-06-27 16:49:03 +0200

    make the form submit, add basic structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,19 @@
-<h1>Hello world!</h1>
+<script lang="ts">
+	import background from "$lib/images/bg-mobile-light.jpg";
+
+	let todo = "";
+  let form: HTMLFormElement;
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Enter") {
+      form.submit();
+    }
+  }
+</script>
+
+<img src={background} alt="background" />
+<h1>Todo</h1>
+<form bind:this={form}>
+	<input type="text" on:keydown={handleKeyDown} bind:value={todo} placeholder="Create a new todo..." />
+</form>
+<p>{todo}</p>
