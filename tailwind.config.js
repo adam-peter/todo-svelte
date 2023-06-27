31406352061fa6e29ffe81b1e93d101388bb1c81
adam.petrzela <adam.petrzela@ais-servis.cz>
2023-06-27 16:31:00 +0200

    initialize tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{html,js,svelte,ts}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					DEFAULT: "hsl(220, 98%, 61%)",
+					"check-background": "linear-gradient(hsl(192, 100%, 67%), hsl(280, 87%, 65%))"
+				},
+				neutral: {
+					"very-light-gray": "hsl(0, 0%, 98%)",
+					"very-light-grayish-blue": "hsl(236, 33%, 92%)",
+					"light-grayish-blue": "hsl(233, 11%, 84%)",
+					"dark-grayish-blue": "hsl(236, 9%, 61%)",
+					"very-dark-grayish-blue": "hsl(235, 19%, 35%)"
+				},
+				light: {
+					"very-light-gray": "hsl(0, 0%, 98%)",
+					"very-light-grayish-blue": "hsl(236, 33%, 92%)",
+					"light-grayish-blue": "hsl(233, 11%, 84%)",
+					"dark-grayish-blue": "hsl(236, 9%, 61%)",
+					"very-dark-grayish-blue": "hsl(235, 19%, 35%)"
+				},
+				dark: {
+					"very-dark-blue": "hsl(235, 21%, 11%)",
+					"very-dark-desaturated-blue": "hsl(235, 24%, 19%)",
+					"light-grayish-blue": "hsl(234, 39%, 85%)",
+					"light-grayish-blue-hover": "hsl(236, 33%, 92%)",
+					"dark-grayish-blue": "hsl(234, 11%, 52%)",
+					"very-dark-grayish-blue": "hsl(233, 14%, 35%)",
+					"very-dark-grayish-blue-two": "hsl(237, 14%, 26%)"
+				}
+			},
+			fontFamily: {
+				"josefin-sans": ["Josefin Sans", "sans-serif"]
+			}
+		}
+	},
+	plugins: []
+};
